c3e1597e0770d82bc598dbf6bff1ad321790c522
Zach Eggert <egger0a6@email.arizona.edu>
2024-05-13 17:44:40 -0400

    complete animation on Footer socials links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,30 +1,74 @@
+"use client";
+
 import { socials } from "@/constants";
 import Section from "./design/Section";
 import Image from "next/image";
+import { backIn, backInOut, backOut, circIn, circInOut, circOut, easeIn, easeInOut, easeOut, motion, stagger, useAnimate, useAnimationControls, useInView, useMotionValue } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 const Footer = () => {
+  const [scope, animate] = useAnimate();
+  const isInView = useInView(scope);
+
+  const [hoverState, setHoverState] = useState(false);
+
+  const handleHover = () => {
+    setHoverState(!hoverState);
+  }
+
+  useEffect(() => {
+    const linkYAnimation = animate(
+      "#link",
+      {y: [0 , -14]},
+      {duration: 1.25, ease: backInOut, delay: stagger(0.3125, { startDelay: 1}), repeat: Infinity, repeatType: "reverse", type: "tween", repeatDelay: .7}
+    );
+    const linkColorAnimation = animate(
+      "#link",
+      {background: ["#1B2027", "#EA5454"]},
+      {duration: 1.25, ease: circInOut, delay: stagger(0.3125, {ease: easeOut,startDelay: 1}), repeat: Infinity, repeatType: "mirror", type: "keyframes", repeatDelay: .7}
+    );
+
+    if (isInView && !hoverState) {
+      linkYAnimation.play();
+      linkColorAnimation.play();
+    } else {
+      linkYAnimation.complete();
+      linkColorAnimation.complete();
+      linkYAnimation.pause();
+      linkColorAnimation.pause();
+    }
+  }, [isInView, hoverState, animate]);
+
   return (
     <Section 
       crosses
       className="!px-0 !py-10"
     >
-      <div className="container flex flex-col justify-center items-center gap-5">
-        {/* TODO: add tooltips and animate socials links */}
+      <div ref={scope} className="container flex flex-col justify-center items-center gap-5">
         <div className="flex flex-wrap gap-5">
           {socials.map((item) => (
-            <a 
+            <motion.div
+              id="link"
               key={item.id}
-              href={item.url}
-              target="_blank"
-              className="flex items-center justify-center w-14 h-14 bg-dark-4 rounded-full transition-colors hover:bg-dark-6"
+              initial={{rotate: 0}}
+              onHoverStart={handleHover}
+              onHoverEnd={handleHover}
+              whileHover={{scale: 1.2, rotate: [30, 0], transition: {duration: 0.5}}}
+              className="flex items-center justify-center w-14 h-14 bg-dark-4 rounded-full "
             >
-              <Image 
-                src={item.iconUrl}
-                alt={item.title}
-                width={28}
-                height={28}
-              />
-            </a>
+              <a
+                href={item.url}
+                target="_blank"
+                
+              >
+                <Image 
+                  src={item.iconUrl}
+                  alt={item.title}
+                  width={28}
+                  height={28}
+                />
+              </a>
+            </motion.div>
           ))}
         </div>
         <p className="font-code text-sm">© {new Date().getFullYear()}. Zach Eggert</p>
