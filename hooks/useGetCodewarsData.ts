56a73682abe18d927fd8fafaff0d6f2d6254a5ea
Zach Eggert <egger0a6@email.arizona.edu>
2024-09-04 15:01:05 -0400

    add file directory for webhooks, add new webhook file useGetCodeWarsData.ts for methods that make API calls to the CodeWars API, add types and methods for getting user and challenges data from codewars api in useGetCodeWarsData.ts

--- hooks/useGetCodewarsData.ts
+++ hooks/useGetCodewarsData.ts
@@ -10,6 +10,31 @@ interface UserTypes {
   skills: string[];
 }
 
+interface RankTypes {
+  rank: string;
+  color: string;
+}
+
+interface ChallengeDataTypes {
+  url: string;
+  category: string;
+  description: string;
+  rank: RankTypes;
+}
+
+interface ChallengeTypes {
+  id: string;
+  name: string;
+  completedLanguages: string[];
+  data: ChallengeDataTypes;
+}
+
+interface ChallengesTypes {
+  totalPages: number;
+  totalItems: number;
+  challenges: ChallengeTypes[];
+}
+
 export const useGetCodewarsData = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [user, setUser] = useState<UserTypes>();
@@ -33,9 +58,30 @@ export const useGetCodewarsData = () => {
       setUser(user);
     }
     getUser();
+
+    const getChallenges = async () => {
+      const resp = await fetch("https://www.codewars.com/api/v1/users/egger0a6/code-challenges/completed");
+      const challengesData = await resp.json();
+      const challengeData = {
+        totalPages: challengesData.totalPages,
+        totalItems: challengesData.totalItems,
+        challenges: [],
+      };
+
+      challengesData.forEach((challenge: any) => {
+        const newChallenge = {
+          id: challenge.id,
+          name: challenge.name,
+          completedLanguages: challenge.completedLanguages,
+          data: {},
+        }
+        challengeData.challenges.push(newChallenge);
+      });
+    }
   }, []);
 
   if (user && challenges) setIsLoading(true);
+  console.log(isLoading)
 
   return {
     user,
