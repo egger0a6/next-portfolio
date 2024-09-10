6536ec85e046dd4ed9cc681bc17836abcac92dc1
Zach Eggert <egger0a6@email.arizona.edu>
2024-09-10 00:29:37 -0400

    complete function in useGetCodewarsData.ts hook to get all completed codewars challenge data for a specific user

--- hooks/useGetCodewarsData.ts
+++ hooks/useGetCodewarsData.ts
@@ -38,7 +38,7 @@ interface ChallengesTypes {
 export const useGetCodewarsData = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [user, setUser] = useState<UserTypes>();
-  const [challenges, setChallenges] = useState();
+  const [challenges, setChallenges] = useState<ChallengesTypes>();
 
   useEffect(() => {
     const getUser = async () => {
@@ -53,7 +53,7 @@ export const useGetCodewarsData = () => {
         honor: userData.honor,
         leaderboard: userData.leaderboardPosition,
         skills: Object.keys(userData.ranks.languages),
-      }
+      };
 
       setUser(user);
     }
@@ -62,21 +62,39 @@ export const useGetCodewarsData = () => {
     const getChallenges = async () => {
       const resp = await fetch("https://www.codewars.com/api/v1/users/egger0a6/code-challenges/completed");
       const challengesData = await resp.json();
-      const challengeData = {
+      const challengeData: ChallengesTypes = {
         totalPages: challengesData.totalPages,
         totalItems: challengesData.totalItems,
         challenges: [],
       };
 
-      // challengesData.forEach((challenge: any) => {
-      //   const newChallenge = {
-      //     id: challenge.id,
-      //     name: challenge.name,
-      //     completedLanguages: challenge.completedLanguages,
-      //     data: {},
-      //   }
-      //   challengeData.challenges.push(newChallenge);
-      // });
+      challengesData.data.forEach((challenge: any) => {
+        const newChallenge: ChallengeTypes = {
+          id: challenge.id,
+          name: challenge.name,
+          completedLanguages: challenge.completedLanguages,
+          data: <ChallengeDataTypes>{},
+        }
+        challengeData.challenges.push(newChallenge);
+      });
+
+      challengeData.challenges.forEach(async (challenge: ChallengeTypes) => {
+        const resp = await fetch(`https://www.codewars.com/api/v1/code-challenges/${challenge.id}`);
+        const challengeDetails = await resp.json();
+        const challengeData: ChallengeDataTypes = {
+          url: challengeDetails.id,
+          category: challengeDetails.category,
+          description: challengeDetails.description,
+          rank: {
+            rank: challengeDetails.rank.name,
+            color: challengeDetails.rank.color,
+          },
+        }
+
+        challenge.data = challengeData;
+      });
+
+      setChallenges(challengeData);
     }
   }, []);
 
